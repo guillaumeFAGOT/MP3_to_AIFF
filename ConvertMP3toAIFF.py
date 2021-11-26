bb8274b708602ac0cd3f75831dd4ce0c39422fe0
guillaumeFAGOT <38430601+guillaumeFAGOT@users.noreply.github.com>
2021-11-26 16:04:21 +0100

    Mise à jour commentaires

--- ConvertMP3toAIFF.py
+++ ConvertMP3toAIFF.py
@@ -1,7 +1,13 @@
+# petite fonction python a utiliser sur machine qui supporte python (de base sur machine issus de Linux.)
+# on importe les libraires python
 import os
 import os.path
-chemin = os.listdir("/home/guillaume/Bureau/ITW")
+# recuperation des fichiers dans le repertoire
+chemin = os.listdir("/home/guillaume/ITW")
 for ligne in chemin:
+	# liste des fichiers existants
 	print (ligne)
-	print ('ffmpeg -y -i ' + '~/Bureau/ITW/'+ligne + ' '  '~/Bureau/ITW/'+ ligne.split('mp3')[0]+'aiff')	
+	# verification de la commande ffmpeg
+	print ('ffmpeg -y -i ' + '~/Bureau/ITW/'+ligne + ' '  '~/Bureau/ITW/'+ ligne.split('mp3')[0]+'aiff')
+	# lancement de la commande
 	os.system('ffmpeg -y -i ' + '~/Bureau/ITW/'+ligne + ' '  '~/Bureau/ITW/'+ ligne.split('mp3')[0]+'aiff')		
